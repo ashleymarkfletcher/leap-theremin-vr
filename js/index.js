1f09c96d7f6c36a779f9f18bfdc27944aba8f2ff
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-08-11 00:32:01 +0100

    changed scale

--- js/index.js
+++ js/index.js
@@ -32,55 +32,55 @@ function draw() {
 
   if (frame.hands.length > 0) {
 
-    frame.hands.forEach((hand) => {
-
-      if (hand.type == "left") {
-        var leftHandY = hand.palmPosition[1]
-
-        console.log('left: ', hand.palmPosition)
-
-        var amp = map(leftHandY, 50, 500, 0, 1)
-
-        var circ = document.querySelector('#left');
-
-        var mappedHand = mapHandToScene(hand.palmPosition)
-        var roll = radToDeg(hand.roll())
-        var pitch = 180-radToDeg(hand.pitch())
-        var yaw = 180-radToDeg(hand.yaw())
-
-        circ.setAttribute('rotation', {x: pitch, y: yaw, z: roll});
-        circ.setAttribute('position', {x: mappedHand[0], y: mappedHand[1], z: mappedHand[2]});
-
-        // console.log('amp: ', amp)
-
-        // osc.amp(amp);
-      } else {
-        var rightHandX = hand.palmPosition[0]
-
-        // console.log('right: ', hand)
-        var mappedHand = mapHandToScene(hand.palmPosition)
-
-    // console.log('rightX: ', rightHandX)
-        // var pitch = map(rightHandX, -300, 300, 0, 1800)
-
-        var roll = radToDeg(hand.roll())
-        var pitch = 180-radToDeg(hand.pitch())
-        var yaw = 180-radToDeg(hand.yaw())
-
-        console.log(radToDeg(hand.roll()));
-
-        var circ = document.querySelector('#right');
-
-
-        circ.setAttribute('rotation', {x: pitch, y: yaw, z: roll});
-        circ.setAttribute('position', {x: mappedHand[0], y: mappedHand[1], z: mappedHand[2]});
-
-        hand.fingers.forEach(function(finger){
-          console.log('finger', finger);
-        });
-        // osc.freq(pitch)
-      }
-    })
+    // frame.hands.forEach((hand) => {
+    //
+    //   if (hand.type == "left") {
+    //     var leftHandY = hand.palmPosition[1]
+    //
+    //     console.log('left: ', hand.palmPosition)
+    //
+    //     var amp = map(leftHandY, 50, 500, 0, 1)
+    //
+    //     var circ = document.querySelector('#left');
+    //
+    //     var mappedHand = mapHandToScene(hand.palmPosition)
+    //     var roll = radToDeg(hand.roll())
+    //     var pitch = 180-radToDeg(hand.pitch())
+    //     var yaw = 180-radToDeg(hand.yaw())
+    //
+    //     circ.setAttribute('rotation', {x: pitch, y: yaw, z: roll});
+    //     circ.setAttribute('position', {x: mappedHand[0], y: mappedHand[1], z: mappedHand[2]});
+    //
+    //     // console.log('amp: ', amp)
+    //
+    //     // osc.amp(amp);
+    //   } else {
+    //     var rightHandX = hand.palmPosition[0]
+    //
+    //     // console.log('right: ', hand)
+    //     var mappedHand = mapHandToScene(hand.palmPosition)
+    //
+    // // console.log('rightX: ', rightHandX)
+    //     // var pitch = map(rightHandX, -300, 300, 0, 1800)
+    //
+    //     var roll = radToDeg(hand.roll())
+    //     var pitch = 180-radToDeg(hand.pitch())
+    //     var yaw = 180-radToDeg(hand.yaw())
+    //
+    //     console.log(radToDeg(hand.roll()));
+    //
+    //     var circ = document.querySelector('#right');
+    //
+    //
+    //     circ.setAttribute('rotation', {x: pitch, y: yaw, z: roll});
+    //     circ.setAttribute('position', {x: mappedHand[0], y: mappedHand[1], z: mappedHand[2]});
+    //
+    //     hand.fingers.forEach(function(finger){
+    //       console.log('finger', finger);
+    //     });
+    //     // osc.freq(pitch)
+    //   }
+    // })
   }
 }
 
@@ -97,20 +97,20 @@ function radToDeg(radians) {
   return Math.round(radians * (180 / Math.PI));
 }
 
-var sceneEl = document.querySelector('a-scene');
-var left = document.createElement('a-box');
-left.setAttribute("id", "left");
-left.setAttribute("color", "#EF2D5E");
-left.setAttribute("width", "4");
-left.setAttribute("depth", "5");
-
-sceneEl.appendChild(left);
-
-var right = document.createElement('a-box');
-right.setAttribute("id", "right");
-right.setAttribute("color", "#EF2D5E");
-right.setAttribute("width", "4");
-right.setAttribute("depth", "5");
-// el.setAttribute('radius', '5');
-
-sceneEl.appendChild(right);
+// var sceneEl = document.querySelector('a-scene');
+// var left = document.createElement('a-box');
+// left.setAttribute("id", "left");
+// left.setAttribute("color", "#EF2D5E");
+// left.setAttribute("width", "4");
+// left.setAttribute("depth", "5");
+//
+// sceneEl.appendChild(left);
+//
+// var right = document.createElement('a-box');
+// right.setAttribute("id", "right");
+// right.setAttribute("color", "#EF2D5E");
+// right.setAttribute("width", "4");
+// right.setAttribute("depth", "5");
+// // el.setAttribute('radius', '5');
+//
+// sceneEl.appendChild(right);
